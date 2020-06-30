70f841d32a7b569c0e32770a87fad2f889d6ab8c
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-06-30 01:35:29 +0100

    remove Tailwind import in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
-import './tailwind.generated.css';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
